531ab7ea04c2347eeb1b6f6df81f3373d6818182
mishka <mishamyagkiy3@gmail.com>
2020-11-04 22:42:17 +0200

    Created: inside main-page - search component and filter, outside - result-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainPageComponent } from './main-page/main-page.component';
+import { SearchSiteComponent } from './main-page/search-site/search-site.component';
+import { FilterComponent } from './main-page/filter/filter.component';
+import { ResultPageComponent } from './result-page/result-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    SearchSiteComponent,
+    FilterComponent,
+    ResultPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-page/search-site/search-site.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchSiteComponent } from './search-site.component';
+
+describe('SearchSiteComponent', () => {
+  let component: SearchSiteComponent;
+  let fixture: ComponentFixture<SearchSiteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SearchSiteComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchSiteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
